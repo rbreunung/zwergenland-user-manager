d5c4b8c2f8a313aa0eb147a98482d05c5c1818c9
Robert Breunung <rbreunung@gmail.com>
2024-09-11 09:04:04 +0200

    excel reader POC in progress

--- src/cli.py
+++ src/cli.py
@@ -1,8 +1,10 @@
 """The CLI entry point of the Zwergenland user manager."""
 
 
-from typing import TextIO
+from typing import Optional, TextIO
 from click import argument, echo, group, option, File
+
+from excel_reader import ExcelReader, KindergardenExcelSheetConfiguration
 
 
 @group()
@@ -28,9 +30,13 @@ def add_user(group_id: str, user_id: str):
 @cli.command()
 @argument('input_file', type=File('r'))
 @option('--first-data-row', default=3, type=int, help="First row in the Excel sheet containing the data.")
-def read_users(input_file: TextIO, first_data_row: int):
+def read_users(input_file: TextIO, first_data_row: Optional[int]):
     """Read user details from Excel."""
+    input_file.close()
     echo(f"Reading from file {input_file.name} and the first data row {first_data_row}")
+    excel_reader = ExcelReader(input_file.name)
+    config = KindergardenExcelSheetConfiguration()
+    excel_reader.read_contacts(config)
 
 
 if __name__ == '__main__':
--- src/excel_reader.py
+++ src/excel_reader.py
@@ -2,26 +2,47 @@
 from typing import List
 import pandas
 
+from user_details import UserDetails
+
 # all row constants are 0 based, while Excel UI is 1 based
-LAST_NAME_1_HEADLINE = 'namem'
 
 
-class KinderGardenExcelSheetConfiguration:  # pylint: disable=R0903
-    """Configuration Model"""
+class AssociationExcelSheetConfiguration:  # pylint: disable=R0903
+    """Association Configuration Model"""
     sheet_id: str | int | None = None
     first_data_row: int | None = None
+    last_name_1_headline = 'name1'
+    # Mutter
+    first_name_1_column: str = 'B'
+    last_name_1_column: str = 'A'
+    email_1_column: str = 'J'
+    phone_1_column: str | None = None
+    # Vater
+    first_name_2_column: str = 'D'
+    last_name_2_column: str = 'C'
+    email_2_column: str = 'K'
+    phone_2_column: str | None = None
+    # Kind
+    child_name: str | None = None
+
+
+class KindergardenExcelSheetConfiguration:  # pylint: disable=R0903
+    """Kindergarden Configuration Model"""
+    sheet_id: str | int | None = None
+    first_data_row: int | None = None
+    last_name_1_headline = 'namem'
     # Mutter
     first_name_1_column: str = 'X'
     last_name_1_column: str = 'W'
     email_1_column: str = 'AM'
-    phone_1_column: str = 'AK'
+    phone_1_column: str | None = 'AK'
     # Vater
     first_name_2_column: str = 'Z'
     last_name_2_column: str = 'Y'
     email_2_column: str = 'AN'
-    phone_2_column: str = 'AL'
+    phone_2_column: str | None = 'AL'
     # Kind
-    child_name: str = 'C'
+    child_name: str | None = 'C'
 
 
 class ExcelReader:
@@ -36,32 +57,32 @@ class ExcelReader:
             index = index * 26 + (ord(char.upper()) - ord('A') + 1)
         return index - 1
 
-    def _get_kindergarden_sheet(self, config: KinderGardenExcelSheetConfiguration) -> pandas.DataFrame:
+    def _get_sheet(self, config: KindergardenExcelSheetConfiguration | AssociationExcelSheetConfiguration) -> pandas.DataFrame:  # pylint: disable=C0301
         """Validate / Complete the configuration and get the sheet."""
         if config.sheet_id is None:
             config.sheet_id = self._file.sheet_names[0]
-        df = self._file.parse(config.sheet_id, header=None)
-        assert df is not None, "sheet should be available here."
+        data_frame = self._file.parse(config.sheet_id, header=None)
+        assert data_frame is not None, "sheet should be available here."
 
         if config.first_data_row is None:
             column_index = self._excel_col_to_index(config.last_name_1_column)
             # Iterate over the DataFrame to find the first row where column matches known headline.
-            for i in range(len(df)):
-                value = df.iat[i, column_index]
-                if value == LAST_NAME_1_HEADLINE:
+            for i in range(len(data_frame)):
+                value = data_frame.iat[i, column_index]
+                if value == config.last_name_1_headline:
                     print(f"First row where column {column_index+1} matches expected headline is: {i+1}")
                     config.first_data_row = i + 1
                     break
 
         assert config.first_data_row is not None, "Expected headline not found in sheet."
 
-        return df
+        return data_frame
 
-    def read_parent_contacts(self, config: KinderGardenExcelSheetConfiguration) -> List[Contact]:
+    def read_contacts(self, config: KindergardenExcelSheetConfiguration | AssociationExcelSheetConfiguration) -> List[UserDetails]:  # pylint: disable=C0301
         """Read the Excel file in the child-parent-format and parse parent contacts."""
         print("\nhello\n")
 
-        sheet = self._get_kindergarden_sheet(config)
+        sheet = self._get_sheet(config)
         assert isinstance(config.first_data_row, int), "First data row is expected to be set here."
         contacts = []
         last_name_1_column_index = self._excel_col_to_index(config.last_name_1_column)
@@ -73,7 +94,3 @@ class ExcelReader:
                 break
             print(f"{cell_value} ")
         return contacts
-
-    def read_member_contacts(self) -> List[Contact]:
-        """Read the excel in the association member format and parse contacts."""
-        return []
